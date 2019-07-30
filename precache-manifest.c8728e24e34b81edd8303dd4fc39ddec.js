c1a53503df5dc1638c1342ec0d5f11695ac579a3
mpvitalik <mpvitalik@gmail.com>
2019-07-30 16:47:51 +0300

    Updates

---
+++ precache-manifest.c8728e24e34b81edd8303dd4fc39ddec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60e0d232e415d3c51e233a42942e15e",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "fb942d6b9e9d4e4ae8e7",
+    "url": "/react_dynamic-list-of-posts/static/css/main.b6d1b06a.chunk.css"
+  },
+  {
+    "revision": "333b5d4033a126b8c391",
+    "url": "/react_dynamic-list-of-posts/static/js/2.4c408244.chunk.js"
+  },
+  {
+    "revision": "fb942d6b9e9d4e4ae8e7",
+    "url": "/react_dynamic-list-of-posts/static/js/main.33efaafa.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
